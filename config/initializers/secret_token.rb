1288e4adb13cfcc17bd26f925637bc5b6d29d29b
mohan kumar <jakkula4@gmail.com>
2012-05-30 01:35:50 -0400

    on linux

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+A2::Application.config.secret_token = '03bd577431901a13fa195889f481109456f26ea1bf93cfda3663f12e2e63f17a33739ec466ac7c314809bd022b8e762527f8a6e0f1f210f5f07b3e5be7f5f331'
